5156285ef554f23d27e4c10bba91441a0359d9fc
3yukus <3yukus@gmail.com>
2025-01-21 07:30:15 +0000

    Submit

---
+++ python_practice/next_year.py
@@ -0,0 +1,7 @@
+print("2,AA")
+name = input("Enter your name :")
+age = int(input("Enter your age :"))
+
+
+print(f"wassup {name}, you're {age} young today.")
+print(f"You'll be {age + 10} years young the next decade.")
